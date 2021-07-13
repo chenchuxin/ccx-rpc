1aaff16bbf75b20d97426a72432e479264f04c6d
chenchuxin <idesireccx@163.com>
2021-07-13 23:03:25 +0800

    feat: ExtensionLoader 初步写完

--- ccx-rpc-common/src/main/java/com/ccx/rpc/common/extension/ExtensionLoader.java
+++ ccx-rpc-common/src/main/java/com/ccx/rpc/common/extension/ExtensionLoader.java
@@ -10,9 +10,14 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * 扩展类加载器。
- * 扩展类的配置写到 META-INF/extensions/ccx-rpc 目录下，文件名为接口全名。
- * 文件格式为：扩展名=扩展类全名。例如：zk=com.ccx.core.registry.zkRegistry
+ * <p>扩展类加载器。</p>
+ * <p>扩展类的配置写到 {@code META-INF/extensions/ccx-rpc} 目录下，文件名为接口全名。</p>
+ * <p>文件格式为：扩展名=扩展类全名。例如：{@code zk=com.ccx.core.registry.ZkRegistry}</p>
+ * <p>获取扩展类实例的代码示例如下：</p>
+ * <pre>{@code
+ *     ExtensionLoader loader = ExtensionLoader.getExtensionLoader(Registry.class)
+ *     Registry registry = loader.getExtension("zk");
+ * }</pre>
  *
  * @author chenchuxin
  * @date 2021/7/12
@@ -20,14 +25,24 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ExtensionLoader<T> {
 
     /**
-     * 扩展缓存
+     * 扩展类实例缓存 {name: 扩展类实例}
      */
     private final Map<String, T> extensionsCache = new ConcurrentHashMap<>();
 
     /**
+     * 扩展加载器实例缓存 {类型：加载器实例}
+     */
+    private final Map<Class<?>, ExtensionLoader<?>> extensionLoaderCache = new ConcurrentHashMap<>();
+
+    /**
+     * 扩展类配置列表缓存 {type: {name, 扩展类}}
+     */
+    private final Map<Class<?>, Map<String, Class<?>>> extensionClassesCache = new ConcurrentHashMap<>();
+
+    /**
      * 扩展类加载器的类型
      */
-    private Class<T> type;
+    private final Class<T> type;
 
     /**
      * 扩展类存放的目录地址
@@ -35,19 +50,34 @@ public class ExtensionLoader<T> {
     private static final String EXTENSION_PATH = "META-INF/extensions/ccx-rpc";
 
     /**
-     * TODO 这里可以做个缓存，各个类型对应各自的加载器
-     *
      * @param type 扩展类加载器的类型
      */
-    public ExtensionLoader(Class<T> type) {
+    private ExtensionLoader(Class<T> type) {
         this.type = type;
+    }
+
+    /**
+     * 获取对应类型的扩展加载器实例
+     *
+     * @param type 扩展类加载器的类型
+     * @return 扩展类加载器实例
+     */
+    public ExtensionLoader<?> getExtensionLoader(Class<?> type) {
+        ExtensionLoader<?> extensionLoader = extensionLoaderCache.get(type);
+        if (extensionLoader != null) {
+            return extensionLoader;
+        }
+        // TODO 线程安全
+        extensionLoader = new ExtensionLoader<>(type);
+        extensionLoaderCache.putIfAbsent(type, extensionLoader);
+        return extensionLoader;
     }
 
     /**
      * 根据名字获取扩展类实例
      *
-     * @param name
-     * @return
+     * @param name 扩展类在配置文件中配置的名字
+     * @return 扩展类实例，如果找不到，则抛出异常
      */
     public T getExtension(String name) {
         // 从缓存中获取
@@ -55,15 +85,17 @@ public class ExtensionLoader<T> {
         if (extension != null) {
             return extension;
         }
-        return createExtension(name);
+        extension = createExtension(name);
+        extensionsCache.putIfAbsent(name, extension);
+        return extension;
     }
 
     /**
      * 创建对应名字的扩展类实例
      * TODO 添加缓存
      *
-     * @param name
-     * @return
+     * @param name 扩展名
+     * @return 扩展类实例
      */
     private T createExtension(String name) {
         // 获取当前类型所有扩展类
@@ -85,10 +117,25 @@ public class ExtensionLoader<T> {
     /**
      * 获取当前类型{@link #type}的所有扩展类
      *
-     * @return {name，clazz}
+     * @return {name: clazz}
      */
     private Map<String, Class<?>> getAllExtensionClasses() {
-        // TODO 加缓存
+        // TODO 线程安全
+        Map<String, Class<?>> extensionClasses = extensionClassesCache.get(type);
+        if (extensionClasses != null) {
+            return extensionClasses;
+        }
+        extensionClasses = loadClassesFromResources();
+        extensionClassesCache.putIfAbsent(type, extensionClasses);
+        return extensionClasses;
+    }
+
+    /**
+     * 从资源文件中加载所有扩展类
+     *
+     * @return {name: 扩展类}
+     */
+    private Map<String, Class<?>> loadClassesFromResources() {
         Map<String, Class<?>> extensionClasses = new ConcurrentHashMap<>();
         // 扩展配置文件名
         String fileName = EXTENSION_PATH + type.getName();
@@ -105,24 +152,37 @@ public class ExtensionLoader<T> {
                         if (line == null) {
                             break;
                         }
-                        line = line.trim();
-                        // TODO: #号开头的注释解析
-                        String[] kv = line.split("=");
-                        if (kv.length != 2 || kv[0].length() == 0 || kv[1].length() == 0) {
-                            throw new IllegalStateException("Extension file parsing error. Invalid format!");
-                        }
-                        if (extensionClasses.containsKey(kv[0])) {
-                            throw new IllegalStateException(kv[0] + " is already exists!");
-                        }
-                        Class<?> clazz = classLoader.loadClass(kv[1]);
-                        extensionClasses.put(kv[0], clazz);
+                        parseLine(line, extensionClasses);
                     }
                 }
             }
         } catch (IOException | ClassNotFoundException e) {
-            // TODO
-            e.printStackTrace();
+            throw new IllegalStateException("Parse file fail." + e.getMessage());
         }
         return extensionClasses;
+    }
+
+    /**
+     * 解析行，并且把解析到的类，放到 extensionClasses 中
+     *
+     * @param line
+     * @param extensionClasses
+     * @throws ClassNotFoundException
+     */
+    private void parseLine(String line, Map<String, Class<?>> extensionClasses) throws ClassNotFoundException {
+        line = line.trim();
+        // 忽略#号开头的注释
+        if (line.startsWith("#")) {
+            return;
+        }
+        String[] kv = line.split("=");
+        if (kv.length != 2 || kv[0].length() == 0 || kv[1].length() == 0) {
+            throw new IllegalStateException("Extension file parsing error. Invalid format!");
+        }
+        if (extensionClasses.containsKey(kv[0])) {
+            throw new IllegalStateException(kv[0] + " is already exists!");
+        }
+        Class<?> clazz = ExtensionLoader.class.getClassLoader().loadClass(kv[1]);
+        extensionClasses.put(kv[0], clazz);
     }
 }
