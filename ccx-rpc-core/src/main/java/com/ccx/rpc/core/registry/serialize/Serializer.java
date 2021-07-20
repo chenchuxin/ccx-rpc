2f1fe1c7e723618b30a8de23a6f9857a1fd09277
chenchuxin <idesireccx@163.com>
2021-07-21 00:29:32 +0800

    feat(serialize): 序列化接口

---
+++ ccx-rpc-core/src/main/java/com/ccx/rpc/core/registry/serialize/Serializer.java
@@ -0,0 +1,31 @@
+package com.ccx.rpc.core.registry.serialize;
+
+import com.ccx.rpc.common.extension.SPI;
+
+/**
+ * 序列化器
+ *
+ * @author chenchuxin
+ * @date 2021/7/20
+ */
+@SPI
+public interface Serializer {
+
+    /**
+     * 序列化
+     *
+     * @param object 要序列化的对象
+     * @return 字节数组
+     */
+    byte[] serialize(Object object);
+
+    /**
+     * 反序列化
+     *
+     * @param bytes 字节数组
+     * @param clazz 要反序列化的类
+     * @param <T>   类型
+     * @return 反序列化的对象
+     */
+    <T> T deserialize(byte[] bytes, Class<T> clazz);
+}
