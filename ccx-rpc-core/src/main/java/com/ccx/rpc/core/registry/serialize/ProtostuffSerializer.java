80395408991fc98215c9215dae58fa16e80b1395
chenchuxin <idesireccx@163.com>
2021-07-22 23:58:59 +0800

    feat(serialize): 添加 protostuff 序列化实现

---
+++ ccx-rpc-core/src/main/java/com/ccx/rpc/core/registry/serialize/ProtostuffSerializer.java
@@ -0,0 +1,35 @@
+package com.ccx.rpc.core.registry.serialize;
+
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+
+/**
+ * https://github.com/protostuff/protostuff
+ *
+ * @author chenchuxin
+ * @date 2021/7/22
+ */
+public class ProtostuffSerializer implements Serializer {
+
+    private static final LinkedBuffer BUFFER = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+
+    @Override
+    public byte[] serialize(Object object) {
+        Schema schema = RuntimeSchema.getSchema(object.getClass());
+        try {
+            return ProtostuffIOUtil.toByteArray(object, schema, BUFFER);
+        } finally {
+            BUFFER.clear();
+        }
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> clazz) {
+        Schema<T> schema = RuntimeSchema.getSchema(clazz);
+        T obj = schema.newMessage();
+        ProtostuffIOUtil.mergeFrom(bytes, obj, schema);
+        return obj;
+    }
+}
